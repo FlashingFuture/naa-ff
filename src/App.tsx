e44692fff220e468dc8c471bc6f93eafde7a375c
FF <wjdrldud98@gmail.com>
2025-03-20 20:35:59 +0900

    Initial Commit

--- src/App.tsx
+++ src/App.tsx
@@ -1,15 +1,14 @@
-import { useState } from "react";
+// import { useState } from "react";
 import "./App.css";
 
 function App() {
-  const [posts] = useState([]);
+  // const [posts] = useState([]);
 
   return (
-    <div style={{ maxWidth: "600px", margin: "0 auto", padding: "20px" }}>
+    <div>
       <h1>Naa</h1>
       <h3>FlashingFuture</h3>
       <p>나아가고, 나아지는 블로그입니다.</p>
-      <hr />
       {/* {posts.map((post, index) => (
         <div
           key={index}
