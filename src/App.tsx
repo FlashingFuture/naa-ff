dd0fad6f973803d8dd5df608d2120eb1c66e4537
FF <wjdrldud98@gmail.com>
2025-03-20 19:19:19 +0900

    Initial commit

--- src/App.tsx
+++ src/App.tsx
@@ -10,15 +10,15 @@ function App() {
       <h3>FlashingFuture</h3>
       <p>나아가고, 나아지는 블로그입니다.</p>
       <hr />
-      {posts.map((post, index) => (
+      {/* {posts.map((post, index) => (
         <div
           key={index}
           style={{ borderBottom: "1px solid #ccc", padding: "10px 0" }}
         >
-          {/* <h2>{post?.title}</h2>
-          <p>{post?.content}</p> */}
+          <h2>{post?.title}</h2>
+          <p>{post?.content}</p>
         </div>
-      ))}
+      ))} */}
     </div>
   );
 }
